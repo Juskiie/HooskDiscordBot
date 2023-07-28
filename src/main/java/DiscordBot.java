51c313974dd3e95046edc1a66683bae16a45139e
Juskiie <juskiehusky@icloud.com>
2023-07-28 14:36:08 +0100

    Update github

--- src/main/java/DiscordBot.java
+++ src/main/java/DiscordBot.java
@@ -1,14 +1,9 @@
-import Hoosk.api.commands.ShutdownCommand;
+import Hoosk.api.commands.TicketCommand;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
 
@@ -17,34 +12,15 @@ public class DiscordBot extends ListenerAdapter {
         final String TOKEN = System.getenv("DISCORD_BOT");
         JDA builder = JDABuilder.createLight(TOKEN, Collections.emptyList())
                 .addEventListeners(new DiscordBot())
-                .addEventListeners(new ShutdownCommand())
-                .setActivity(Activity.playing("Type /ping"))
+                .setActivity(Activity.playing("Type /ticket"))
                 .build()
                 .awaitReady();
 
         builder.updateCommands().addCommands(
                 Commands.slash("ping", "Calculate ping of the bot"),
-                Commands.slash("ban", "Ban a user from the server")
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.BAN_MEMBERS)) // only usable with ban permissions
-                        .setGuildOnly(true) // Ban command only works inside a guild
-                        .addOption(OptionType.USER, "user", "The user to ban", true) // required option of type user (target to ban)
-                        .addOption(OptionType.STRING, "reason", "The ban reason"), // optional reason
                 Commands.slash("ticket", "Send a support ticket!")
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ALL_TEXT_PERMISSIONS))
         ).queue();
-    }
 
-    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
-        // make sure we handle the right command
-        if (event.getName().equals("ping")) {
-            long time = System.currentTimeMillis();
-            event.reply("Pong!").setEphemeral(true) // reply or acknowledge
-                    .flatMap(v ->
-                            event.getHook().editOriginalFormat("Pong: %d ms", System.currentTimeMillis() - time) // then edit original
-                    ).queue(); // Queue both reply and edit
-        } else if (event.getName().equals("ticket")){
-            // TODO
-
-        }
+        builder.addEventListener(new TicketCommand());
     }
 }
