ca2f3520ab7476b1cac6b31a8e1984062365b247
Juskiie <juskiehusky@icloud.com>
2023-08-01 13:53:46 +0100

    Update project

--- src/main/java/DiscordBot.java
+++ src/main/java/DiscordBot.java
@@ -7,8 +7,14 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
 import java.util.Collections;
 
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+
 public class DiscordBot extends ListenerAdapter {
-    public static void main(String[] args) throws InterruptedException {
+
+    public static void main(String[] args) throws InterruptedException{
         final String TOKEN = System.getenv("DISCORD_BOT");
         JDA builder = JDABuilder.createLight(TOKEN, Collections.emptyList())
                 .addEventListeners(new DiscordBot())
@@ -22,5 +28,32 @@ public class DiscordBot extends ListenerAdapter {
         ).queue();
 
         builder.addEventListener(new TicketCommand());
+    }
+
+    @SuppressWarnings("unused")
+    public static String getSecret() {
+        String secretName = "hooskbot/api/key";
+        Region region = Region.of("eu-west-2");
+
+        SecretsManagerClient client = SecretsManagerClient.builder()
+                .region(region)
+                .build();
+
+        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                .secretId(secretName)
+                .build();
+
+        GetSecretValueResponse getSecretValueResponse;
+
+        try {
+            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+        } catch (Exception e) {
+            // For a list of exceptions thrown, see
+            // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+            e.printStackTrace();
+            throw e;
+        }
+
+        return getSecretValueResponse.secretString();
     }
 }
